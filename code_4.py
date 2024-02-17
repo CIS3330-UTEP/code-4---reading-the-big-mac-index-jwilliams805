17cd608968886d316c839d81daf2616dc8be7649
jwilliams805 <jiwillams@miners.utep.edu>
2024-02-17 11:28:02 -0700

    Explaing changes

--- code_4.py
+++ code_4.py
@@ -7,40 +7,47 @@ def get_big_mac_price_by_year(year,country_code):
     pass
     query_year = df[(df['iso_a3']== country_code.upper()) & (df['date'].str.startswith(str(year)))]
     rmp_year = round(query_year ['dollar_price'].mean(),2)
-    print (rmp_year)
+    # print (rmp_year)
     return rmp_year
 
 def get_big_mac_price_by_country(country_code):
     pass
     query_country = df[df['iso_a3'] == country_code.upper()]
     rmp_country = round(query_country ['dollar_price'].mean(),2)
-    print (rmp_country)
+    # print (rmp_country)
     return rmp_country
 
 def get_the_cheapest_big_mac_price_by_year(year):
     pass
     query_cheapest = df[df['date'].str.startswith(str(year))]
+    # filter the dataframe for the specified year
     cheap_row = query_cheapest.loc[query_cheapest['dollar_price'].idxmin()]
+    # find the row with the minimum 'dollar_price'
+    # .loc goes through the query and finds the lowest price 
     name_of_country = cheap_row['name']
     country_code = cheap_row['iso_a3']
     dollar_price = cheap_row[ 'dollar_price']
-    print (f"{name_of_country}({country_code}): ${dollar_price:.2f}")
+    # Extract relevant information from the row 
+    # print (f"{name_of_country}({country_code}): ${dollar_price:.2f}")
     return f"{name_of_country}({country_code}): ${dollar_price:.2f}"
 
 def get_the_most_expensive_big_mac_price_by_year(year):
     pass
     query_mexpensive = df[df['date'].str.startswith(str(year))]
-    cheap_row = query_mexpensive.loc[query_mexpensive['dollar_price'].idxmax()]
-    name_of_country = cheap_row['name']
-    country_code = cheap_row['iso_a3']
-    dollar_price = cheap_row[ 'dollar_price']
-    print (f"{name_of_country}({country_code}): ${dollar_price:.2f}")
+    # filter the dataframe for the specified year
+    expensive_row = query_mexpensive.loc[query_mexpensive['dollar_price'].idxmax()]
+    # find the row with the maximum 'dollar_price'
+    # .loc goes through the query and finds the highest price 
+    name_of_country = expensive_row['name']
+    country_code = expensive_row['iso_a3']
+    dollar_price = expensive_row[ 'dollar_price']
+    # Extract relevant information from the row 
+    # print (f"{name_of_country}({country_code}): ${dollar_price:.2f}")
     return f"{name_of_country}({country_code}): ${dollar_price:.2f}"
 
 if __name__ == "__main__":
-    # pass
-    get_big_mac_price_by_year(str(input("Enter year: ")), input('Enter country code: '))
-    get_big_mac_price_by_country((input("Enter country code: ")))
-    get_the_cheapest_big_mac_price_by_year(str(input('Enter year: ')))
-    get_the_most_expensive_big_mac_price_by_year(str(input('Enter year: ')))
-    
+    pass
+    # get_big_mac_price_by_year(str(input("Enter year: ")), input('Enter country code: '))
+    # get_big_mac_price_by_country((input("Enter country code: ")))
+    # get_the_cheapest_big_mac_price_by_year(str(input('Enter year: ')))
+    # get_the_most_expensive_big_mac_price_by_year(str(input('Enter year: ')))
