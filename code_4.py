99aa285481760224e347169ebbf40144c1296652
jwilliams805 <jiwillams@miners.utep.edu>
2024-02-16 21:28:38 -0700

    Finishing last 2 making minor fixes

--- code_4.py
+++ code_4.py
@@ -7,20 +7,20 @@ def get_big_mac_price_by_year(year,country_code):
     pass
     query_year = df[(df['iso_a3']== country_code.upper()) & (df['date'].startswith(year))]
     rmp_year = round(query_year ['dollar_price'].mean(),2)
-    print (rmp_year)
+    # print (rmp_year)
     return rmp_year
 
 def get_big_mac_price_by_country(country_code):
     pass
     query_country = df[df['iso_a3'] == country_code.upper()]
     rmp_country = round(query_country ['dollar_price'].mean(),2)
-    print (rmp_country)
+    # print (rmp_country)
     return rmp_country
 
 def get_the_cheapest_big_mac_price_by_year(year):
     pass
     query_cheapest = df[df['date'].startswith(year)]
-    cheap_row = query_cheapest.iterrows([query_cheapest['dollar_price'].idxmin()])
+    cheap_row = query_cheapest.loc[[query_cheapest['dollar_price'].idxmin()]]
     name_of_country = cheap_row['name']
     country_code = cheap_row['iso_a3']
     dollar_price = cheap_row[ 'dollar_price']
@@ -29,7 +29,7 @@ def get_the_cheapest_big_mac_price_by_year(year):
 def get_the_most_expensive_big_mac_price_by_year(year):
     pass
     query_cheapest = df[df['date'].startswith(year)]
-    cheap_row = query_cheapest.iterrows([query_cheapest['dollar_price'].idxmax()])
+    cheap_row = query_cheapest.loc[[query_cheapest['dollar_price'].idxmax()]]
     name_of_country = cheap_row['name']
     country_code = cheap_row['iso_a3']
     dollar_price = cheap_row[ 'dollar_price']
@@ -38,4 +38,4 @@ def get_the_most_expensive_big_mac_price_by_year(year):
 if __name__ == "__main__":
     # pass
     get_big_mac_price_by_country((input("Enter country code: ")))
-    # get_big_mac_price_by_year(int(input('Enter year: ')),str(input("Enter country code: ")))
+    
